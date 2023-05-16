79e801ffdd0ab59f9e5a55e2545ae5435b7485bd
sidneymaldonado <sidneymaldonado@gmail.com>
2023-05-16 19:42:37 -0400

    Analisador Semantico

---
+++ src/AnalisadorSemantico.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnalisadorSemantico {
+
+	public List<String> tabelaSimbolos;
+	public AnalisadorSemantico() {
+		tabelaSimbolos = new ArrayList();
+	}
+	
+	public void Analisar(String linha, List<String> dicionario) {
+		linha = linha.replace(";","");
+		String[] token = linha.split(" ");
+		if (token[0].equals("inteiro") ||
+			token[0].equals("int")	) {
+			// adicionar na tabela de simbolos
+			tabelaSimbolos.add(token[1]);
+			return;
+		}
+		// percorre todas as palavras da linha
+		for(int i=0;i<token.length;i++) {
+			// verificar se é uma variável
+			if (ehVariavel(token[i].toString(), dicionario)) {
+				if (estaNaTabelaSimbolos(token[i].toString())) {
+					return;
+				} else {
+					System.out.println("Variavel nao declarada:"+token[i]);
+				}
+			}
+		}
+	}
+	
+	public Boolean ehVariavel(String palavra, List<String> dicionario) {
+		
+		if(palavra.matches("[a-z][a-z0-9]{0,9}")) {
+			for(int i=0; i<dicionario.size();i++) {
+				if (dicionario.get(i).toString().equals(palavra)) {
+					return false;
+				}
+			}
+			return true;			
+		}
+		return false;
+	}
+	public Boolean estaNaTabelaSimbolos(String palavra) {
+		
+		for(int i=0;i<tabelaSimbolos.size();i++) {
+			if (tabelaSimbolos.get(i).toString().equals(palavra)){
+				return true;
+			}
+		}
+		return false;
+	}
+}
--- src/Compilador.java
+++ src/Compilador.java
@@ -6,6 +6,7 @@ public class Compilador {
 	public List<String> alfabeto;
 	public List<String> exemplo;
 	public List<String> expressoes;
+	public List<String> dicionario;
 	
 	public Compilador() {
 		// TODO Auto-generated constructor stub
@@ -20,6 +21,7 @@ public class Compilador {
 		alfabeto = leitor.ler("C:\\Users\\Aluno\\eclipse-workspace\\CBR\\alfabeto.txt");
 		exemplo = leitor.ler("C:\\Users\\Aluno\\eclipse-workspace\\CBR\\exemplo.txt");
 		expressoes = leitor.ler("C:\\Users\\Aluno\\eclipse-workspace\\CBR\\expressoes.txt");
+		dicionario = leitor.ler("C:\\Users\\Aluno\\eclipse-workspace\\CBR\\dicionario.txt");
 		for( int i =0; i < alfabeto.size();i++) {
 			System.out.println(alfabeto.get(i));
 		}
@@ -35,6 +37,15 @@ public class Compilador {
 			linha = exemplo.get(i);
 			sintatico.analisar(linha, expressoes);
 		}
+		
+		System.out.println("Analisando semanticamente");
+		AnalisadorSemantico semantico = new AnalisadorSemantico();
+		for( int i =0; i < exemplo.size();i++) 
+		{
+			linha = exemplo.get(i);
+			semantico.Analisar(linha, dicionario);
+		}
+		System.out.println("Fim da analise semantica");
 
 	}
 
